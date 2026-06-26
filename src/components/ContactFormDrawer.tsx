185df241d3aae5cf7c551c5de812efb26c90ae7a
qw8555jin-alt <stu01dio@gmail.com>
2026-06-26 10:58:01 +0900

    fix: change form POST endpoint to static file

--- src/components/ContactFormDrawer.tsx
+++ src/components/ContactFormDrawer.tsx
@@ -36,7 +36,7 @@ export default function ContactFormDrawer() {
     }
     
     try {
-      const response = await fetch('/', {
+      const response = await fetch('/__forms.html', {
         method: 'POST',
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: searchParams.toString()
